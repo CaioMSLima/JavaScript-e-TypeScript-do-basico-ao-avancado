5f1f69e1955c72ee96cd9daf24f07f472d8caec2
CaioMSLima <caiomslima@gmail.com>
2023-05-21 18:12:12 -0300

    commit 14

---
+++ 3-JavaScript Básico/6-operators/operators.js
@@ -0,0 +1,24 @@
+// arithmeti + - / *
+// potentiatio **
+// rest of Division %
+
+const num1 = 42
+const num2 = 30
+const num3 = 2
+const string1 = "30"
+
+console.log(num1 + num2)
+console.log(num1 + string1)
+console.log(num2 ** num3)
+console.log(num1 % num2)
+console.log(((num1 + num2)*(num1-num2))/num3)
+
+let counter = 1
+let value = 10
+value *= 10
+
+console.log(++ counter)
+console.log(counter++)
+console.log(counter)
+console.log(-- counter)
+console.log(value)
---
+++ 3-JavaScript Básico/7-convert to number/convert.js
@@ -0,0 +1,8 @@
+// convert to number
+let num4 = parseInt("30")
+let num5 = parseFloat("5.2")
+let num6 = Number("20.02")
+
+console.log( num4 + 3 ,typeof num4 )
+console.log(num5 + 3,typeof num5 )
+console.log(num6 + 3,typeof num6)
