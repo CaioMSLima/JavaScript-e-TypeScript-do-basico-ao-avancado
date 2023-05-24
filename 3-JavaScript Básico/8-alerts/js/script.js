31b5774292ac77051bef90010b663402d16b493c
CaioMSLima <caiomslima@gmail.com>
2023-05-24 19:39:50 -0300

    commit 15

---
+++ 3-JavaScript Básico/8-alerts/js/script.js
@@ -0,0 +1,15 @@
+// Return undefine
+alert("Intruder alert ")
+
+//Return bolean
+//YOu dont need put this in variable
+let cofirma = confirm("Dou you wish delet?")
+
+// the type is string
+let name1 = prompt("type your name")
+
+let number1 = prompt("Type one number")
+let number2 = prompt("Type other number")
+let result = Number(number1) + parseInt(number2)
+
+alert(`The result of this count was: ${result}`)
---
+++ 3-JavaScript Básico/9-Exercice/exercice.js
@@ -0,0 +1,14 @@
+let varA = "A"; //B
+let varB = "B"; //C
+let varC = "C";//A
+
+// varD = varC
+// varC = varA
+// varA = varB;
+// varB =  varD
+//or
+
+[varA,varB, varC] = [varB, varC, varA]
+
+console.log(varA, varB, varC)
+
