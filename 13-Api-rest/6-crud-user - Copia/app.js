c48aeecdf85b15d19a7d1c5c3ff472afdcf7c74e
Caio Lima <caio.lima@backlgrs.com.br>
2024-04-30 18:55:02 -0300

    commit 60

---
+++ 13-Api-rest/6-crud-user - Copia/app.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv';
+
+// this do the .env function
+dotenv.config();
+
+// this import the index
+import './src/database';
+
+import express from 'express';
+import homeRoutes from './src/routes/homeRoutes';
+import userRoutes from './src/routes/userRoutes';
+
+class App {
+  constructor() {
+    this.app = express();
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.app.use(express.urlencoded({ extended: true }));
+    this.app.use(express.json());
+  }
+
+  routes() {
+    this.app.use('/', homeRoutes);
+    this.app.use('/users/', userRoutes);
+  }
+}
+
+export default new App().app;
---
+++ 13-Api-rest/6-crud-user - Copia/src/database/migrations/20240307224205-students.js
@@ -0,0 +1,50 @@
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  // criate the table on my data base
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('students', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      last_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      height: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      Weight: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('students');
+  },
+};
